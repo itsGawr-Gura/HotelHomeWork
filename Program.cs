f574ea564ae0ebb74bb7fb600859dc36becadb1c
itsGawr-Gura <itsgawrgura@gmail.com>
2024-04-03 19:42:59 +0300

    1

--- Program.cs
+++ Program.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
 
 namespace Hotel
 {
     internal static class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary> 
@@ -18,6 +20,12 @@ namespace Hotel
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new HotelMainPage());
+            logger.Trace("trace message");
+            logger.Debug("debug message");
+            logger.Info("info message");
+            logger.Warn("warn message");
+            logger.Error("error message");
+            logger.Fatal("fatal message");
         }
     }
 }
